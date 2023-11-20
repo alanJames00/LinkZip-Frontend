8d6b18fb97124ad456c194017c43b9fea2230438
alanjames00 <alanjames6282@gmail.com>
2023-11-20 19:26:09 +0530

    design homepage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -6,14 +6,13 @@ export default function HomePage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+        LinkZip:<br className="hidden sm:inline" />
+          Elevate Your Links, Minimize the Noise
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
-          Preconfigured Setup for ShadCN with ReactJS
+        Shorten URLs seamlessly, create elegant link trees—minimal effort, maximum impact for your online presence.
         </p>
-        <p className=" text-2xl">Preconf by alanjames00</p>
-        <p>Checkout my GitHub</p>
+      
       </div>
       <div className="flex gap-4">
       
