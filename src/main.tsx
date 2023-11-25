dafeee766afc1c7f33dcf7f3ad87c946218d745c
alanjames00 <alanjames6282@gmail.com>
2023-11-26 01:39:10 +0530

    add all primary routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import { Layout } from 'lucide-react'
+import ShortenUrl from './components/ShortenUrl.tsx'
+import HomePage from './components/HomePage.tsx'
+import CreateLinkTree from './components/createLinkTree.tsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-
+      <Route path='' element={<HomePage />} />
+      <Route path='/shorten' element={<ShortenUrl />} />
+      <Route path='/createTree' element={<CreateLinkTree />} />
     </Route>
   )
 )
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} />  
   </React.StrictMode>,
 )
