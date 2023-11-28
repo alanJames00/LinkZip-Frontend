6da568079710cdfd4a13b59bdf22c64861b07ece
alanjames00 <alanjames6282@gmail.com>
2023-11-28 21:57:57 +0530

    add error message for urlnotfound comp

---
+++ src/components/UrlNotFound.tsx
@@ -0,0 +1,13 @@
+
+export default function UrlNotFound() {
+
+    return (
+        <>
+            <h3>The Requested URL Is Not Found</h3>
+            <ul>
+                <li>Check for Typos in the URL.</li>
+                <li>If you want to create a ShortURL or LinkTree, Use The Above Navigation Bar.</li>
+                </ul>
+        </>
+    )
+}
