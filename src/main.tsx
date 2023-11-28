97e81da5dd72d9395ce8ba4f656a0f0c0d60045b
alanjames00 <alanjames6282@gmail.com>
2023-11-28 23:30:14 +0530

    ,

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Route, RouterProvider, createHashRouter, createRoutesFromElements } from 'react-router-dom'
 import ShortenUrl from './components/ShortenUrl.tsx'
 import HomePage from './components/HomePage.tsx'
 import CreateLinkTree from './components/createLinkTree.tsx'
 import UrlNotFound from './components/UrlNotFound.tsx'
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-      <Route path='' element={<HomePage />} />
+      <Route path='/' element={<HomePage />} />
       <Route path='/shorten' element={<ShortenUrl />} />
       <Route path='/createTree' element={<CreateLinkTree />} />
       <Route path='/urlNotFound' element={<UrlNotFound />} />
