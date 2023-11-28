344afaab272b783e39088eb3515dd7e82ee79d36
alanjames00 <alanjames6282@gmail.com>
2023-11-28 17:09:55 +0530

    add customurl toggle btn

--- src/components/shortenURLForm.tsx
+++ src/components/shortenURLForm.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { ResultCard } from "./shortUrlResult";
+import { ResultCard } from "./shortUrlResult"
+
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
 export default function ShortenUrlForm() {
 
     const [url, setUrl] = useState("");
+    const [isCustomUrl, setIsCustomUrl ] = useState(false);
+    const [customUrl, setCustomUrl] = useState("");
     
     function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
@@ -28,13 +33,32 @@ export default function ShortenUrlForm() {
             value={url}
             onChange={(e) => setUrl(e.target.value)}
             placeholder="Enter the URL To Shorten" 
-            className=" w-[100%] my-10"/>
-
+            className=" w-[100%] my-2"/>
+            
+            
+            <div className="flex items-center space-x-2 mb-10 my-3" >
+              <Switch id="airplane-mode"
+              onClick={() => setIsCustomUrl(!isCustomUrl)} 
+              />
+               <Label htmlFor="airplane-mode">Use Custom ShortURL</Label>
+             </div >
+            
+            <Input type="text" 
+            value={customUrl}
+            onChange={(e) => setCustomUrl(e.target.value)}
+            placeholder="Enter the custom URL" 
+            className=" w-[100%] my-6"
+            style={{marginLeft:0}}
+            disabled={!isCustomUrl}
+            />
+     
+        
             <Button
             type="submit" 
             className=" px-[20%] mb-10"
             >Shorten</Button>
 
+        
 
             <ResultCard /> 
 
