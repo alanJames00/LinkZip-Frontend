99b8ac3134d60a74c1a2cb0d48c76fefd35025db
alanjames00 <alanjames6282@gmail.com>
2023-11-28 12:20:26 +0530

    fix input comp not resolving

--- src/components/shortenURLForm.tsx
+++ src/components/shortenURLForm.tsx
@@ -15,7 +15,10 @@ export default function ShortenUrlForm() {
 
             <form onSubmit={handleSubmit}>
             
-
+            <div className="flex w-full max-w-sm items-center space-x-2">
+            <Input type="email" placeholder="Email" />
+            <Button type="submit">Subscribe</Button>
+            </div>
 
             </form>
         </div>
