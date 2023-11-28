94a69cdfcd01282032b36feeb905b577610a1d03
alanjames00 <alanjames6282@gmail.com>
2023-11-28 16:09:42 +0530

    clean code

--- src/components/shortUrlResult.tsx
+++ src/components/shortUrlResult.tsx
@@ -8,7 +8,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
 import { useState } from "react"
