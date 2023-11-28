b4b1d6957c672635f238366d021bdbb343a2f0b5
alanjames00 <alanjames6282@gmail.com>
2023-11-28 15:59:11 +0530

    clean code

--- src/components/shortUrlResult.tsx
+++ src/components/shortUrlResult.tsx
@@ -10,13 +10,7 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+
 
 export function ResultCard() {
   return (
